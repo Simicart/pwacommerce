350bfc8d92e9867ec40c6037d952da842d65996e
peter <peter@simicart.com>
2018-04-13 16:58:28 +0700

    update build htaccess

--- sw.js
+++ sw.js
@@ -1,4 +1,58 @@
 'use strict';
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        self.skipWaiting()
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['v2'];
+
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    if (event.request.method !== 'POST' &&
+        event.request.url.toString() &&
+        event.request.url.toString().indexOf('/checkout/') === -1 &&
+        event.request.url.toString().indexOf('/cart/') === -1 &&
+        event.request.url.toString().indexOf('/key/') === -1) {
+        event.respondWith(
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response;
+                    }
+                    var fetchRequest = event.request.clone();
+
+                    return fetch(fetchRequest).then(
+                        function (response) {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+
+                            var responseToCache = response.clone();
+                            caches.open('simipwa-cache')
+                                .then(function (cache) {
+                                    cache.put(event.request, responseToCache);
+
+                                });
+
+                            return response;
+                        }
+                    );
+                })
+        );
+    }
+});
 self.addEventListener('push', function(event) {
     var apiPath = './simipwa/index/message?endpoint=';
     event.waitUntil(
